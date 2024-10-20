2286d3c476a34c7fc6aa6240c4a10e672e56dcea
Asma <Asmamutairi11@gmail.com>
2024-10-20 16:26:18 +0300

    Initial Commit

---
+++ Journaling/JournalingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalingApp.swift
+//  Journaling
+//
+//  Created by Asma Mohammed on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournalingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournalingTests/JournalingTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalingTests.swift
+//  JournalingTests
+//
+//  Created by Asma Mohammed on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Journaling
+
+struct JournalingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
