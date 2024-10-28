84eccbb88236e18024fa9b6e8a546d236657607a
Asma <Asmamutairi11@gmail.com>
2024-10-28 15:04:34 +0300

    28/Oct

---
+++ Journaling/Empty State.swift
@@ -0,0 +1,18 @@
+//
+//  Empty State.swift
+//  Journaling
+//
+//  Created by Asma Mohammed on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Empty_State: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Empty_State()
+}
---
+++ sheet.swift
@@ -0,0 +1,18 @@
+//
+//  sheet.swift
+//  Journaling
+//
+//  Created by Asma Mohammed on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct sheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sheet()
+}
