1e462106aa35c21e778f5250f7dda0f4d7159709
Asma <Asmamutairi11@gmail.com>
2024-11-03 15:02:57 +0300

    Last update 3/Nov

---
+++ Journaling/modling.swift
@@ -0,0 +1,16 @@
+//
+//  modling.swift
+//  Journaling
+//
+//  Created by Asma Mohammed on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+
+struct modling: Identifiable {
+    var id: ObjectIdentifier
+    
+    let gg: UUID = .init()
+}
+
